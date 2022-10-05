635b7e05bf96e988327d787f32443f37f3edd79b
mayowa1305 <adeyinka.boluwatife17@gmail.com>
2022-10-05 12:22:43 +0100

    fixed authorization error on transfer api

--- api/transfers.go
+++ api/transfers.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/mayowa1305/simpleBank/db/sqlc"
+	"github.com/mayowa1305/simpleBank/token"
 )
 
 type transferRequest struct {
@@ -22,10 +25,19 @@ func (server *Server) createTransfers(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	if !server.validAccount(ctx, req.FromAccountNumber, req.Currency) {
+
+	fromAccount, valid := server.validAccount(ctx, req.FromAccountNumber, req.Currency)
+	if !valid {
 		return
 	}
-	if !server.validAccount(ctx, req.ToAccountNumber, req.Currency) {
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account does not belong to authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	_, valid = server.validAccount(ctx, req.ToAccountNumber, req.Currency)
+	if !valid {
 		return
 	}
 	arg := db.TransferTxParams{
@@ -43,21 +55,21 @@ func (server *Server) createTransfers(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, result)
 }
 
-func (server *Server) validAccount(ctx *gin.Context, AccountNumber int64, currency string) bool {
+func (server *Server) validAccount(ctx *gin.Context, AccountNumber int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, AccountNumber)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.AccountNumber, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
 	}
-	return true
+	return account, true
 }
